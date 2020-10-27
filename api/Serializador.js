c346e72e6550bd699dc7c83c7877e249740e9eeb
marcoavfcc01121979 <marcoavfcc@gmail.com>
2020-10-27 18:06:38 -0300

    subtraindo estoque

--- api/Serializador.js
+++ api/Serializador.js
@@ -68,6 +68,19 @@ class SerializadorFornecedor extends Serializador {
   }
 }
 
+class SerializadorProduto extends Serializador {
+  constructor(contentType, camposExtras) {
+    super()
+    this.contentType = contentType
+    this.camposPublico = [
+      'id',
+      'titulo'
+    ].concat(camposExtras || [])
+    this.tagSingular = 'produto'
+    this.tagPlural = 'produtos'
+  }
+}
+
 class SerializadorErro extends Serializador {
   constructor (contentType, camposExtras) {
     super()
@@ -85,5 +98,6 @@ module.exports = {
   Serializador: Serializador,
   SerializadorFornecedor: SerializadorFornecedor,
   SerializadorErro: SerializadorErro,
+  SerializadorProduto: SerializadorProduto,
   formatosAceitos: ['application/json', 'application/xml']
 }
--- api/routes/fornecedores/produtos/index.js
+++ api/routes/fornecedores/produtos/index.js
@@ -3,12 +3,15 @@ const Tabela = require('./TabelaProduto')
 
 const router = Router({ mergeParams: true })
 const Produto = require('./Produto');
-
+const { SerializadorProduto } = require('../../../Serializador')
 
 router.get('/', async (req, res) => {
   const produtos = await Tabela.listar(req.fornecedor.id)
+  const serializador = new SerializadorProduto(
+    res.getHeader('Content-Type')
+  )
   res.send(
-    JSON.stringify(produtos)
+    serializador.serializar(produtos)
   )
 })
 
@@ -19,8 +22,13 @@ router.post('/', async (req, res, next) => {
     const dados = Object.assign({}, corpo, { fornecedor: idFornecedor });
     const produto = new Produto(dados);
     await produto.criar()
+    const serializador = new SerializadorProduto(
+      res.getHeader('Content-Type')
+    )
     res.status(201);
-    res.send(produto);
+    res.send(
+      serializador.serializar(produto)
+    );
   }catch(erro) {
     next(erro);
   }
@@ -36,6 +44,63 @@ router.delete('/:id', async (req, res) => {
   await produto.apagar()
   res.status(204)
   res.end()
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const dados = {
+      id: req.params.id,
+      fornecedor: req.fornecedor.id
+    }
+  
+    const produto = new Produto(dados)
+    await produto.carregar()
+    const serializador = new SerializadorProduto(
+      res.getHeader('Content-Type'),
+      ['preco', 'estoque', 'fornecedor', 'dataCriacao', 'dataAtualizacao', 'versao']
+    )
+    res.send(
+      serializador.serializar(produto)
+    )
+  
+  }catch(erro){
+    next(erro)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const dados = Object.assign(
+      {},
+      req.body,
+      {
+        id: req.params.id,
+        fornecedor: req.fornecedor.id
+      }
+    )
+    const produto = new Produto(dados)
+    await produto.atualizar()
+    res.status(204)
+    res.end()
+  } catch(erro) {
+    next(erro)
+  }
+})
+
+router.post('/:id/diminuir-estoque', async (req, res, next) => {
+  try{
+    const produto = new Produto({
+      id: req.params.id,
+      fornecedor: req.fornecedor.id
+    })
+    await produto.carregar()
+    produto.estoque = produto.estoque - req.body.quantidade
+    await produto.diminuirEstoque()
+    res.status(204)
+    res.end()
+  }catch(erro) {
+    next(erro)
+  }
 })
 
 module.exports = router
